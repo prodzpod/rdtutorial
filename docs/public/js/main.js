ac4539b0286b1e29bf83102fc71a81716e0d3fa3
BuildTools <unconfigured@null.spigotmc.org>
2021-05-24 16:49:57 +0900

    fix 8.5, an experiment

--- docs/public/js/main.js
+++ docs/public/js/main.js
@@ -9,6 +9,19 @@ i = 0;
 function init() {
     maxpage = Math.floor((document.querySelectorAll('.endmenu-element').length + 3) / 4);
     var clipboard = new ClipboardJS(".rdzip");
+
+    clipboard.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+    
+        e.clearSelection();
+    });
+    
+    clipboard.on('error', function (e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+    });
 }
 
 function endmenuleft() {
